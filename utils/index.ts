cb20e63456152d798aa655f22c57203d865e4379
bambang-ap <adhyt.scott@gmail.com>
2026-01-02 10:47:22 +0700

    get greetings utils

--- utils/index.ts
+++ utils/index.ts
@@ -1,2 +1,10 @@
 export * from './cn';
 export * from './navigator';
+
+export function getGreeting(date = new Date()) {
+	const hour = date.getHours();
+	if (hour >= 18 || hour < 5) return 'Good Night';
+	else if (hour >= 15) return 'Good Evening';
+	else if (hour >= 12) return 'Good Afternoon';
+	else return 'Good Morning';
+}
